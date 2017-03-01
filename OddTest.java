8a333c42157678f657a262217f366c01e116ee35
dtihonov982 <dtihonov982@gmail.com>
2017-03-01 09:49:08 +0300

    Добавление решений упражнений

---
+++ OddTest.java
@@ -0,0 +1,37 @@
+/* 
+https://www3.ntu.edu.sg/home/ehchua/programming/java/J2a_BasicsExercises.html
+
+Exercise (Method): Write a boolean method called isOdd() in a class called OddTest, which takes an int as input and returns true if the it is odd. The signature of the method is as follows:
+
+public static boolean isOdd(int number)
+
+Also write the main() method that prompts user for a number, and prints "ODD" or "EVEN". You should test for negative input. 
+*/
+
+import java.util.Scanner;
+
+class OddTest {
+    public static boolean isOdd(int number) {
+        if (number % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter the number: ");
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        
+        if (number < 0) {
+            System.out.println("Number must be positive");
+            System.exit(1);
+        }
+        
+        if (isOdd(number)) {
+            System.out.println("ODD");
+        } else {
+            System.out.println("EVEN");
+        }
+    }
+}
---
+++ ReverseString.java
@@ -0,0 +1,43 @@
+/* 
+https://www3.ntu.edu.sg/home/ehchua/programming/java/J2a_BasicsExercises.html
+
+Exercise ReverseString: Write a program called ReverseString, which prompts user for a String, and prints the reverse of the String. The output shall look like:
+
+Enter a String: abcdef
+The reverse of the String "abcdef" is "fedcba". 
+*/
+
+import java.util.Scanner;
+
+class ReverseString {
+    public static String recReverse(String str) {
+        int length = str.length();
+        if (length < 2) {
+            return str;
+        } else {
+            //Вернуть последний символ + остальные символы в обратном порядке
+            return 
+                str.charAt(length - 1) +
+                recReverse(str.substring(0, length - 1));
+        }
+    }
+    public static String reverse(String str) {
+        //Создается массив размером равным строке и заполняется из строки в обратном порядке.
+        char[] revChars = new char[str.length()];
+        int j = 0;
+        for (int i = str.length() - 1; i >= 0; --i) {
+            revChars[j] = str.charAt(i);
+            j++;
+        }
+        return new String(revChars);           
+    }
+    
+    public static void main(String[] args) {
+        System.out.print("Enter a String: ");
+        Scanner s = new Scanner(System.in);
+        String inStr = s.nextLine();
+
+        System.out.format("The reverse of the String \"%s\" is \"%s\"%n", inStr, reverse(inStr));
+        System.out.format("The recursive reverse of the String \"%s\" is \"%s\"%n", inStr, recReverse(inStr));
+    }
+}
